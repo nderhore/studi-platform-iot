5e2f2bb48c3d0dbe0a37880d1d456777d1813ef0
Nathan Derhore <nathan@derhore.fr>
2022-12-01 16:37:28 +0100

    fix launch app

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ list_device: list = []
 # un get , renvoie une donnée
 @app.route('/', methods=['GET'])
 def get_all_devices():  # put application's code here
-    return json.dumps(list_device)
+    return json.dumps(list_device,default= Device.to_json)
 
 
 # le POST il va crée une donnée
@@ -20,11 +20,11 @@ def get_all_devices():  # put application's code here
 def create_object():
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
-        data: Device = json.loads(request.data)
-        monDevice: Device = Device(data['id'], data['nom'], data['marque'], data['type'])
-        list_device.append(monDevice)
-        return True
-    return False
+        print(request.get_json())
+        data: Device = Device.from_json(request.get_json())
+        list_device.append(data)
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
 
 
 # le PUT modifie un objet
@@ -32,22 +32,22 @@ def create_object():
 def modify_object():
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
-        data = json.loads(request.data)
-        monDevice: Device = Device(data['id'], data['nom'], data['marque'], data['type'])
-        delete_object(monDevice.id)
-        list_device.append(monDevice)
-        return True
-    return False
+        data : Device = json.loads(request.data)
+        delete_object(data.id_device)
+        list_device.append(data)
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
 
 
-@app.route('/<id>', methods=['DELETE'])
-def delete_object(id: int):
+@app.route('/<id_device>', methods=['DELETE'])
+def delete_object(id_device: int):
+    print(id_device)
     for device in list_device:
-        print(list_device)
-        if device.id == id:
+        print(device)
+        if int(device.id_device) == int(id_device):
             list_device.remove(device)
-            break
-
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
 
 if __name__ == '__main__':
     app.run()
